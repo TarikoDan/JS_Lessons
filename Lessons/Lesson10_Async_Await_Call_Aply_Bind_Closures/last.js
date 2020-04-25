b302749542506d4a1f5e65c04a07f736e92776c2
TarikoDan <62624974+TarikoDan@users.noreply.github.com>
2020-04-25 05:07:11 +0300

    without ****

---
+++ Lessons/Lesson10_Async_Await_Call_Aply_Bind_Closures/last.js
@@ -0,0 +1,67 @@
+// Створити карту користувача UserCard
+class Operation {
+    constructor(type,credits,time){
+        this.type = type;
+        this.credits = credits;
+        this.time = time
+    }
+}
+let userCard = function (num) {
+    const key = num;
+    let balance = 100;
+    let transactionLimit = 100;
+    let historyLogs = [];
+    let transactionsCount = 0;
+
+    let getCardOptions = function () {
+        let cardInfo =  {key,balance,transactionLimit,transactionsCount,historyLogs};
+        console.log(`Card${key}`,cardInfo);
+        return cardInfo
+    };
+    let putCredits = function (credit) {
+        balance += credit;
+        transactionsCount++;
+        let oparation = new Operation('putCredits',credit,new Date().toLocaleString());
+        historyLogs.push(oparation)
+
+    };
+    let takeCredits = function (credit) {
+        if (balance > credit) {
+            if (transactionsCount < transactionLimit) {
+                balance -= credit;
+                transactionsCount++;
+                let oparation = new Operation('takeCredits',credit,new Date().toLocaleString());
+                historyLogs.push(oparation)
+            }else {console.warn('Transactions limit has been exhausted, \n' +
+                'try to enlarge the limit...')}
+        }else {console.warn('not enough money, Put Credits...')}
+    };
+    let setTransactionsLimit = function (limit) {
+        transactionLimit = limit;
+        let oparation = new Operation('setTransactionsLimit',limit,new Date().toLocaleString());
+        historyLogs.push(oparation)
+    };
+    let transferCredits = function (credit,cardX) {
+        if (balance > credit*1.05) {
+            if (transactionsCount < transactionLimit) {
+                cardX.putCredits(credit);
+                this.takeCredits(credit*1.05);
+            }else {console.warn('Transactions limit has been exhausted, \n' +
+                'try to enlarge the limit...')}
+        }else {console.warn('not enough money, Put Credits...')}
+    };
+    let card = {getCardOptions,putCredits,takeCredits,setTransactionsLimit,transferCredits};
+
+    return card
+};
+
+let card2 = userCard(2);
+card2.putCredits(1000);
+card2.takeCredits(100);
+card2.setTransactionsLimit(10);
+let card1 = userCard(1);
+card2.transferCredits(500,card1);
+card1.getCardOptions();
+card2.getCardOptions();
+console.log('*******************************************');
+

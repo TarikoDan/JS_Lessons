51ce9bfdd6b3ba35ba5f8e002c9783673b787c5c
TarikoDan <62624974+TarikoDan@users.noreply.github.com>
2020-04-26 14:47:44 +0300

    with 2

--- Lessons/Lesson10_Async_Await_Call_Aply_Bind_Closures/last.js
+++ Lessons/Lesson10_Async_Await_Call_Aply_Bind_Closures/last.js
@@ -6,16 +6,15 @@ class Operation {
         this.time = time
     }
 }
-let userCard = function (num) {
-    const key = num;
+let userCard = function (userName,numCard) {
+    const key = numCard;
     let balance = 100;
     let transactionLimit = 100;
     let historyLogs = [];
     let transactionsCount = 0;
-
     let getCardOptions = function () {
         let cardInfo =  {key,balance,transactionLimit,transactionsCount,historyLogs};
-        console.log(`Card${key}`,cardInfo);
+        console.log(`User- ${userName}: Card${key}`,cardInfo);
         return cardInfo
     };
     let putCredits = function (credit) {
@@ -55,71 +54,103 @@ let userCard = function (num) {
     return card
 };
 
-let card2 = userCard(2);
-card2.putCredits(1000);
-card2.takeCredits(100);
-card2.setTransactionsLimit(10);
-let card1 = userCard(1);
-card2.transferCredits(500,card1);
-card1.getCardOptions();
-card2.getCardOptions();
-console.log('*******************************************');
-
-let card3 = userCard(3);
-
-function card3Take1 ()  {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            card3.takeCredits(1000);
-            resolve (card3.getCardOptions());
-        },1000)
-    })
-};
-let card3SetLimit = () => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            (card3.setTransactionsLimit(2));
-            resolve (card3.getCardOptions())
-
-        },2000)
-    })
-};
-let card3Put = () => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            (card3.putCredits(1000));
-            resolve (card3.getCardOptions())
-
-        },3000)
-    })
-};
-let card3Take2 = () => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            (card3.takeCredits(1000));
-            resolve (card3.getCardOptions())
-
-        }, 4000)
-    })
-};
-async function oneDayCard3 () {
-    card3.getCardOptions();
-    try {
-         await card3Take1();
-         await card3SetLimit();
-         await card3Put();
-         await card3Take2()
+class UserAccount {
+    constructor(name) {
+        this.name = name;
+        this.cards = new Array(3)
     }
-    catch (err) {
-        console.warn(err)
+    addCard(num){
+        if (this.cards[num-1]) {
+            console.warn('Сard with this number already exists!')
+        }else{
+            this.cards[num-1] = userCard(this.name,num);
+            this.cards.length = 3
+        }
+
+
     }
-    await card3Take1();
-    await card3SetLimit();
-    await card3Put();
-    await card3Take2();
-    console.log('************************************')
+    getCardByKey(num) {
+        if (this.cards[num-1]) {
+            return this.cards[num-1]
+        }else{
+            console.warn('No card with this number!')
+        }
+    }
 }
-oneDayCard3();  // ??? Чому блок кетч запускає функції? а не просто відловлює помилки...спрацьовує лічильник.
+let tod = new UserAccount('Tod');
+
+console.log(tod);
+tod.addCard(1);
+const todCard1 = tod.getCardByKey(1);
+todCard1.putCredits(1000);
+console.log('ad1');
+todCard1.getCardOptions();
+console.log(tod);
+tod.addCard(2);
+const todCard2 = tod.getCardByKey(2);
+console.log('ad2');
+todCard2.putCredits(1000);
+todCard2.transferCredits(500,todCard1);
+todCard1.getCardOptions();
+todCard2.getCardOptions();
+tod.addCard(4)
+console.log(tod)
+console.log('*******************************************************')
+console.log('*******************************************************')
+
+// function card3Take1 ()  {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             card3.takeCredits(1000);
+//             resolve (card3.getCardOptions());
+//         },1000)
+//     })
+// }
+// let card3SetLimit = () => {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             (card3.setTransactionsLimit(2));
+//             resolve (card3.getCardOptions())
+//
+//         },2000)
+//     })
+// };
+// let card3Put = () => {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             (card3.putCredits(1000));
+//             resolve (card3.getCardOptions())
+//
+//         },3000)
+//     })
+// };
+// let card3Take2 = () => {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             (card3.takeCredits(1000));
+//             resolve (card3.getCardOptions())
+//
+//         }, 4000)
+//     })
+// };
+// async function oneDayCard3 () {
+//     card3.getCardOptions();
+//     try {
+//          await card3Take1();
+//          await card3SetLimit();
+//          await card3Put();
+//          await card3Take2()
+//     }
+//     catch (err) {
+//         console.warn(err)
+//     }
+//     await card3Take1();
+//     await card3SetLimit();
+//     await card3Put();
+//     await card3Take2();
+//     console.log('************************************')
+// }
+// oneDayCard3();  // ??? Чому блок кетч запускає функції? а не просто відловлює помилки...спрацьовує лічильник.
                 // ??? Чому некоректна поведінка card3SetLimit - чому він запускається першим?
 // card3Take1()
 //     .then(card3SetLimit)
